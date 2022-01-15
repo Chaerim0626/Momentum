00c202eac55e27fcc7be3cab11e0872cfff1830d
Chaerim Park <dasapcr@gmail.com>
2022-01-15 18:08:18 +0900

    0115 upload

--- app.js
+++ app.js
@@ -3,24 +3,3 @@
 //querySelector는 element를 css방식으로 검색
 
 
-const title = document.querySelector(".hello:first-child h1");
-
-
-
-function handleTitleClick() {
-    title.style.color = "blue"; //js에서 style 변경하기
-    console.log("title was clicked!");
-}
-
-function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
-}
-
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave)
