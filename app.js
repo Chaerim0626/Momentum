30012a6cd3eb80254d0e671623c89524f03135a0
Chaerim Park <dasapcr@gmail.com>
2022-01-16 13:45:29 +0900

    0116 upload2

--- app.js
+++ app.js
@@ -5,14 +5,22 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"; //string포함된 변수는 대문자로 표기
+
 
 
 function onLoginSubmit(event) {
     //console.log(loginInput.value); input 내용 가져오기
     
-    event.preventDefault(); //유저가 이름을 form을 통해 제출했을 때 기본동작 막기
-    console.log(loginInput.value);
-
+    event.preventDefault(); //브라우저 기본동작 막기
+    
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
+    const username = loginInput.value; //입력한 값을 username 변수에 넣기
+    
+    greeeting.innerText = `Hello ${username}`; //greeting id에 text추가
+    greeeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
