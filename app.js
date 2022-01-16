9efc149d052575a68b4da60e368aa2e748271ced
Chaerim Park <dasapcr@gmail.com>
2022-01-16 13:31:13 +0900

    0116 upload

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 //querySelector는 element를 css방식으로 검색
 
 
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+
+function onLoginSubmit(event) {
+    //console.log(loginInput.value); input 내용 가져오기
+    
+    event.preventDefault(); //유저가 이름을 form을 통해 제출했을 때 기본동작 막기
+    console.log(loginInput.value);
+
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
