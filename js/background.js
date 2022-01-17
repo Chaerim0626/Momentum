e8ef4ca29144255535b4febbd9a1531a51ecaa2d
Chaerim Park <dasapcr@gmail.com>
2022-01-17 16:48:14 +0900

    0117 upload4

--- js/background.js
+++ js/background.js
@@ -1,8 +1,12 @@
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
+//images array에서 랜덤하게 length개만큼 추출
 const chosenImage = images[Math.floor(Math.random() * quotes.length)];
 
+
+//js에서 html element 생성
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-console.log(bgImage);
+
+document.body.appendChild(bgImage);
