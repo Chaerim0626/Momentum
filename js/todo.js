f75bf6af1145cd144a9763d75868ef9065489e72
Chaerim Park <dasapcr@gmail.com>
2022-01-18 18:48:39 +0900

    0118 upload2

--- js/todo.js
+++ js/todo.js
@@ -1,16 +1,37 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const toDos = [];
 
+
+function saveToDos() {
+    //array에 toDos string으로 바꿔서 집어넣기
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function paintToDO(newTodo){
 
     const li = document.createElement("li");
+
+    //span생성 후 내용 넣기
     const span = document.createElement("span");
+    span.innerText = newTodo;
+
+    //button생성 후 내용 넣기
+    const button = document.createElement("button");
+    button.innerText = "❎";
+    button.addEventListener("click", deleteToDo);
+
 
     //자식으로 span넣기
     li.appendChild(span);
-    span.innerText = newTodo;
+    li.appendChild(button);
+
     toDoList.appendChild(li);
 }
 
@@ -21,7 +42,11 @@ function handleToDoSubmit(event) {
     //입력된 값 새로운 변수에 복사
     const newTodo = toDoInput.value;
     toDoInput.value= "";
+
+    toDos.push(newTodo);
     paintToDO(newTodo);
+
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
